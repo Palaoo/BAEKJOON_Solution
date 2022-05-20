eb80ad4f82bdc730fadecc149f0f894e420bfed9
Palaoo <cjh70105@naver.com>
2022-05-20 15:59:00 +0900

    committed uncompleted Solution1811

--- Solution1181.java
+++ Solution1181.java
@@ -1,39 +1,95 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 
 public class Solution1181 {
+	static StringBuilder sb = new StringBuilder();
+	static Node[] length = new Node[51];
 
 	public static void main(String[] args) throws Exception {
 		int N = read();
 		String str;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		LinkedList<String> stringList = new LinkedList<String>();
-		int compare;
-		stringList.
+		Solution1181 t = new Solution1181();
 
-		str = br.readLine();
 		for (int i = 0; i < N; i++) {
-			if ((compare = compareTo(str.getBytes(), str.length(), (str = br.readLine()).getBytes(), str.length())) > 0)
-				
-				str = br.readLine();
-		}
-	}
-
-	public static int compareTo(byte[] value, int len1, byte[] other, int len2) {
-		int lim = Math.min(len1, len2);
-		for (int k = 0; k < lim; k++) {
-			if (value[k] != other[k]) {
-				return getChar(value, k) - getChar(other, k);
+			str = br.readLine();
+			if (length[str.length()] == null)
+				length[str.length()] = t.new Node(str);
+			else if (length[str.length()] != null) {
+				length[str.length()] = length[str.length()].compareTo(str);
 			}
 		}
-		return len1 - len2;
+
+		for (int i = 1; i < 51; i++) {
+			if (length[i] != null)
+				length[i].append();
+		}
+
+		System.out.print(sb);
+	}
+
+	class Node {
+		String data;
+		Node next;
+
+		Node(String data) {
+			this.data = data;
+			this.next = null;
+		}
+
+		Node(String data, Node next) {
+			this.data = data;
+			this.next = next;
+		}
+
+		void append() {
+			sb.append(this.data + "\n");
+			if (this.next != null)
+				this.next.append();
+		}
+
+		Node compareTo(String other) {
+			byte[] thisB = this.data.getBytes(), otherB = other.getBytes();
+			Node newN = length[other.length()];
+			for (int k = 0; k < other.length(); k++) {
+				if (thisB[k] > otherB[k]) {
+					if (0 == this.search(length[other.length()], this.data, (newN = new Node(other, this))))
+						return length[other.length()];
+					else
+						return newN;
+				} else if (thisB[k] == otherB[k]) {
+					continue;
+				}
+				else {
+					if (this.next != null)
+						return this.next.compareTo(other);
+					else if (this.next == null)
+						this.next = new Node(other);
+				}
+			}
+
+			return newN;
+		}
+
+		int search(Node prev, String data, Node next) {
+			int result = 0;
+			if (prev.data == data) {
+				return result = 1;
+			} else if (prev.next.data == data) {
+				prev.next = next;
+				return result = 0;
+			} else
+				search(prev.next, data, next);
+			return result;
+
+		}
 	}
 
 	static int read() throws Exception {
 		int c, n = System.in.read() & 15;
 		while ((c = System.in.read()) > 32)
 			n = (n << 3) + (n << 1) + (c & 15);
+		System.in.skip(1);
 		return n;
 	}
 }
